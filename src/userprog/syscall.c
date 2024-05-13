3fdfa217b74d78970e8317eb4bf324ace1ea7400
YoussifKhaled <youssif13abdalaziz@gmail.com>
2024-05-13 21:25:27 +0300

    Add system call handling in syscall_handler function

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,58 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
+  int Sys_call = *(int *)f->esp;
+  switch(Sys_call){
+    case SYS_HALT:
+      halt();
+      break;
+    case SYS_EXIT:
+    {
+      int status;
+      status = *(int *)(f->esp + 4);
+      exit(status);
+      break;
+    }
+    case SYS_WAIT:
+    {
+      tid_t pid; 
+      pid = *(tid_t *)(f->esp + 4);
+      f->eax = wait(pid);
+      break;
+    }
+    case SYS_EXEC:
+    {
+      //dah Ay klam ya Ahmed ya mostafa e3ml 4o8lk
+      const char *cmd_line;
+      cmd_line = *(const char **)(f->esp + 4);
+      f->eax = process_execute(cmd_line);
+      break;
+    }
+      break;
+    //do rest of the system calls
+    default:
+      break;
+  }
   thread_exit ();
+}
+
+void halt(void){
+  printf("halt()\n");
+  shutdown_power_off();
+}
+
+void exit(int status){
+  struct semaphore *sema = thread_current()->parent->sema;
+  thread_current()->exit_status = status;
+  sema_up(sema);
+  
+  printf("exit(%d)\n", status);
+  thread_exit();
+}
+
+int wait(tid_t pid){
+  return process_wait(pid);
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,18 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <list.h>
+#include "threads/thread.h"
+#include "threads/synch.h"
+#include "threads/interrupt.h"
+#include "threads/vaddr.h"
+#include "process.h"
+#include "devices/shutdown.h"
+#include "devices/input.h"
+
 void syscall_init (void);
+void halt(void);
+void exit(int status);
+int wait(tid_t pid);
 
 #endif /* userprog/syscall.h */
